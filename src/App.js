493307a4607f143572e5c647c5afe3cc4a8ba3aa
JosephEddy <jer@esokia-webagency.com>
2022-06-29 11:08:52 +0300

    mycommit4

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [text1, setText] = useState('')
+  const [text2, setText] = useState('')
   return (
     <div className="App">
       <input 
-        onChange={(text1) => setText(text1.target.value)}
+        onChange={(text2) => setText(text2.target.value)}
       />
-      <p>{text1}</p>
+      <p>{text2}</p>
     </div>
   );
 }
