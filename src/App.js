9d7b98844ee8228815e86ca90a6436010666e787
JosephEddy <jer@esokia-webagency.com>
2022-06-30 09:57:21 +0300

    my commit5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Item from "./content/Item";
 
 function App() {
   const [text2, setText] = useState('')
@@ -9,6 +10,7 @@ function App() {
         onChange={(text2) => setText(text2.target.value)}
       />
       <p>{text2}</p>
+      <Item name="Joseph" firstname="Eddy" nationalite="malagasy"/>
     </div>
   );
 }
---
+++ src/content/Item.js
@@ -0,0 +1,9 @@
+const Item = (props) => {
+    return (
+        <div>
+            <h1>from item {props.name} {props.firstname}</h1>
+        </div>
+    )
+}
+
+export default Item;
